d0948083113079eae576c715baae7de8e4e3829d
togami <tuabobo123@gmail.com>
2020-07-03 22:02:36 +0900

    add swiper

--- src/js/media.js
+++ src/js/media.js
@@ -1,12 +1,10 @@
 const media = () => {
-  const cards = document.querySelector(".media_carousels");
-
   const mediaContent = document.querySelector("ul.media_carousels-content");
-
   for (let i = 0; i < 10; i++) {
-    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+    mediaContent.appendChild(mediaContent.children[i].cloneNode(true));
   }
 };
-setInterval(media(), 60000);
+media();
+//setInterval(media(),300)
 
-export default media;
+// export default media;
